956b46c5b2b6ee9cf317fda2165d267bd3d8d2cd
Eduardo Moises <eduardomoises@MMM00928BRZ.local>
2025-01-16 13:56:56 -0300

    criacao  e configuracao de arquivos de pipeline

--- cypress.config.js
+++ cypress.config.js
@@ -5,18 +5,34 @@ module.exports = defineConfig({
   e2e: {
     specPattern: 'cypress/e2e/**/*.cy.js',
     baseUrl: 'https://front.serverest.dev/',
-    viewportWidth: 1280, 
-    viewportHeight: 720, 
+    viewportWidth: 1280,
+    viewportHeight: 720,
     setupNodeEvents(on, config) {
       // Chama o plugin do Allure corretamente
       allureWriter(on, config);
       
       // A função de configuração original do Cypress continua
       require('@shelex/cypress-allure-plugin').addPlugin(on, config);
+
+      // Configuração personalizada para o navegador Firefox
+      if (config.browser.name === 'firefox') {
+        config.firefoxGcInterval = 1000;
+        config.chromeOptions = {
+          args: ['--headless', '--no-sandbox']
+        };
+      }
+
+      // Configuração para o navegador Chrome (não é necessária alteração, mas pode ser personalizada aqui)
+      if (config.browser.name === 'chrome') {
+        config.chromeOptions = {
+          args: ['--headless', '--no-sandbox']
+        };
+      }
+
       return config;
     },
     env: {
-      allureResultsPath: 'cypress/allure-results', 
+      allureResultsPath: 'cypress/allure-results',
     },
   },
 });
