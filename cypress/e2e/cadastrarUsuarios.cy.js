db3e2b31b64e343c143289af7b1a128ab63b68ea
Eduardo Moises <eduardomoises@MMM00928BRZ.local>
2025-01-16 17:01:52 -0300

    configuracao wiat para firefox, esta se perdendo mas chamadas  por timeout as vezes

--- cypress/e2e/cadastrarUsuarios.cy.js
+++ cypress/e2e/cadastrarUsuarios.cy.js
@@ -7,33 +7,23 @@ describe('Cadastro de Usuário', () => {
     cy.fixture('usuarios.json').then((usuarios) => {
       const tiposUsuarios = Object.keys(usuarios);
       const tipoUsuarioAleatorio = tiposUsuarios[Math.floor(Math.random() * tiposUsuarios.length)];
-  
+
       const usuario = usuarios[tipoUsuarioAleatorio];
       const nome = usuario.nome;
       const email = usuario.email;
       const password = usuario.password;
-  
-      // Intercepta a requisição de cadastro
+
       cy.intercept('POST', '/usuarios').as('cadastroRequest');
-  
-      // Executa o cadastro do usuário
       cy.cadastrarUsuario(nome, email, password);
-  
-      // Verifica se a URL foi alterada corretamente após o cadastro
-      cy.url().should('eq', 'https://front.serverest.dev/home');
-  
-      // Captura um screenshot do sucesso do cadastro
+
+      cy.url().should('include', '/cadastrarusuarios');
+      cy.url({ timeout: 20000 }).should('include', '/home');
       cy.screenshot(`cadastro_usuario_${tipoUsuarioAleatorio}_sucesso`);
-  
-      // Remove o usuário utilizado do fixture
+
       delete usuarios[tipoUsuarioAleatorio];
-  
-      // Atualiza o arquivo de fixture
       cy.writeFile('cypress/fixtures/usuarios.json', usuarios);
     });
   });
-  
-
 
   it('Não permite cadastrar um usuário com email que já existe na base', () => {
     cy.intercept('POST', 'https://serverest.dev/login').as('loginRequest');
@@ -45,10 +35,8 @@ describe('Cadastro de Usuário', () => {
     const password = "admin123";
 
     cy.cadastrarUsuario(nome, email, password);
-
     cy.contains('Este email já está sendo usado').should('be.visible');
   });
-
 
   it('Não permite cadastrar usuário sem preencher campos obrigatórios', () => {
     cy.intercept('POST', 'https://serverest.dev/login').as('loginRequest');
@@ -56,11 +44,8 @@ describe('Cadastro de Usuário', () => {
     cy.intercept('GET', 'https://serverest.dev/produtos').as('produtosRequest');
 
     cy.submeterCadastro();
-
-
     cy.contains('Nome é obrigatório').should('be.visible');
     cy.contains('Email é obrigatório').should('be.visible');
     cy.contains('Password é obrigatório').should('be.visible');
   });
-
 });
