658f2ad9affcca9d7ae5e9830a1b4390e7234138
Eduardo Moises <eduardomoises@MMM00928BRZ.local>
2025-01-16 14:48:20 -0300

    criacao  e configuracao de arquivos de pipeline

--- cypress/e2e/cadastrarUsuarios.cy.js
+++ cypress/e2e/cadastrarUsuarios.cy.js
@@ -1,6 +1,6 @@
 describe('Cadastro de Usuário', () => {
   beforeEach(() => {
-    cy.visit('/cadastrarusuarios'); // Acessa a página de cadastro antes de cada teste
+    cy.visit('/cadastrarusuarios'); 
   });
 
   it('Deve cadastrar um usuário com dados válidos', () => {
@@ -9,25 +9,31 @@ describe('Cadastro de Usuário', () => {
     cy.intercept('GET', 'https://serverest.dev/produtos').as('produtosRequest');
 
     cy.fixture('usuarios.json').then((usuarios) => {
-      const tiposUsuarios = Object.keys(usuarios); // Pega as chaves (tipos de usuários)
-      const tipoUsuarioAleatorio = tiposUsuarios[Math.floor(Math.random() * tiposUsuarios.length)]; // Escolhe um tipo aleatório de usuário
+      const tiposUsuarios = Object.keys(usuarios); 
+      const tipoUsuarioAleatorio = tiposUsuarios[Math.floor(Math.random() * tiposUsuarios.length)]; 
 
-      const usuario = usuarios[tipoUsuarioAleatorio]; // Pega os dados do usuário escolhido aleatoriamente
+      const usuario = usuarios[tipoUsuarioAleatorio]; 
       const nome = usuario.nome;
       const email = usuario.email;
       const password = usuario.password;
 
-      cy.cadastrarUsuario(nome, email, password); // Realiza o cadastro do usuário com os dados gerados
+      cy.cadastrarUsuario(nome, email, password); 
 
-      cy.wait('@loginRequest').its('response.statusCode').should('eq', 200); // Verifica a resposta da requisição de login
-      cy.wait('@usuariosRequest').its('response.statusCode').should('eq', 200); // Verifica a resposta da requisição de usuários
-      cy.wait('@produtosRequest').its('response.statusCode').should('eq', 200); // Verifica a resposta da requisição de produtos
+      cy.wait('@loginRequest').its('response.statusCode').should('eq', 200); 
+      cy.wait('@usuariosRequest').its('response.statusCode').should('eq', 200);
+      cy.wait('@produtosRequest').its('response.statusCode').should('eq', 200); 
 
-      cy.url().should('eq', 'https://front.serverest.dev/home'); // Verifica se a URL foi redirecionada corretamente para a página inicial
+      cy.url().should('eq', 'https://front.serverest.dev/home'); 
 
-      cy.screenshot(`cadastro_usuario_${tipoUsuarioAleatorio}_sucesso`); // Tira uma screenshot para validar o sucesso do cadastro
+      cy.screenshot(`cadastro_usuario_${tipoUsuarioAleatorio}_sucesso`); 
+
+  
+      delete usuarios[tipoUsuarioAleatorio]; 
+ 
+      cy.writeFile('cypress/fixtures/usuarios.json', usuarios); 
     });
   });
+
 
   it('Não permite cadastrar um usuário com email que já existe na base', () => {
     cy.intercept('POST', 'https://serverest.dev/login').as('loginRequest');
