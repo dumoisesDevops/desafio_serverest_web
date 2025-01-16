c4a854b45942dca8e30690a57900543045bf6901
Eduardo Moises <eduardomoises@MMM00928BRZ.local>
2025-01-16 14:19:06 -0300

    criacao  e configuracao de arquivos de pipeline

--- cypress.config.js
+++ cypress.config.js
@@ -3,36 +3,35 @@ const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 
 module.exports = defineConfig({
   e2e: {
-    specPattern: 'cypress/e2e/**/*.cy.js',
-    baseUrl: 'https://front.serverest.dev/',
-    viewportWidth: 1280,
-    viewportHeight: 720,
+    specPattern: 'cypress/e2e/**/*.cy.js',  // Define o padrão dos testes
+    baseUrl: 'https://front.serverest.dev/',  // URL base do site para os testes
+    viewportWidth: 1280,  // Largura da tela para os testes
+    viewportHeight: 720,  // Altura da tela para os testes
+
     setupNodeEvents(on, config) {
-      // Chama o plugin do Allure corretamente
+      // Chama o plugin do Allure para gerar os relatórios
       allureWriter(on, config);
-      
-      // A função de configuração original do Cypress continua
+      // Adiciona o plugin do Allure
       require('@shelex/cypress-allure-plugin').addPlugin(on, config);
 
       // Configuração personalizada para o navegador Firefox
       if (config.browser.name === 'firefox') {
         config.firefoxGcInterval = 1000;
-        config.chromeOptions = {
-          args: ['--headless', '--no-sandbox']
-        };
       }
 
-      // Configuração para o navegador Chrome (não é necessária alteração, mas pode ser personalizada aqui)
+      // Configuração para navegadores com opções específicas
       if (config.browser.name === 'chrome') {
         config.chromeOptions = {
-          args: ['--headless', '--no-sandbox']
+          args: ['--headless', '--no-sandbox'],
         };
       }
 
+      // Retorna a configuração do Cypress com as alterações aplicadas
       return config;
     },
+
     env: {
-      allureResultsPath: 'cypress/allure-results',
+      allureResultsPath: 'cypress/allure-results',  // Caminho dos resultados do Allure
     },
   },
 });
