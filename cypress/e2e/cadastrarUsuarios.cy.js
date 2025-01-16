386bd949cdd6dfece340a1a5af02a3f37339c7ae
Eduardo Moises <eduardomoises@MMM00928BRZ.local>
2025-01-16 14:57:40 -0300

    criacao  e configuracao de arquivos de pipeline

--- cypress/e2e/cadastrarUsuarios.cy.js
+++ cypress/e2e/cadastrarUsuarios.cy.js
@@ -1,36 +1,36 @@
 describe('Cadastro de Usuário', () => {
   beforeEach(() => {
-    cy.visit('/cadastrarusuarios'); 
+    cy.visit('/cadastrarusuarios');
   });
 
   it('Deve cadastrar um usuário com dados válidos', () => {
-    cy.intercept('POST', 'https://serverest.dev/login').as('loginRequest');
+    cy.intercept('POST', 'https://serverest.dev/login', { statusCode: 200, body: { success: true } }).as('loginRequest');
     cy.intercept('GET', 'https://serverest.dev/usuarios').as('usuariosRequest');
     cy.intercept('GET', 'https://serverest.dev/produtos').as('produtosRequest');
 
     cy.fixture('usuarios.json').then((usuarios) => {
-      const tiposUsuarios = Object.keys(usuarios); 
-      const tipoUsuarioAleatorio = tiposUsuarios[Math.floor(Math.random() * tiposUsuarios.length)]; 
+      const tiposUsuarios = Object.keys(usuarios);
+      const tipoUsuarioAleatorio = tiposUsuarios[Math.floor(Math.random() * tiposUsuarios.length)];
 
-      const usuario = usuarios[tipoUsuarioAleatorio]; 
+      const usuario = usuarios[tipoUsuarioAleatorio];
       const nome = usuario.nome;
       const email = usuario.email;
       const password = usuario.password;
 
-      cy.cadastrarUsuario(nome, email, password); 
+      cy.cadastrarUsuario(nome, email, password);
 
-      cy.wait('@loginRequest').its('response.statusCode').should('eq', 200); 
+      cy.wait('@loginRequest').its('response.statusCode').should('eq', 200);
       cy.wait('@usuariosRequest').its('response.statusCode').should('eq', 200);
-      cy.wait('@produtosRequest').its('response.statusCode').should('eq', 200); 
+      cy.wait('@produtosRequest').its('response.statusCode').should('eq', 200);
 
-      cy.url().should('eq', 'https://front.serverest.dev/home'); 
+      cy.url().should('eq', 'https://front.serverest.dev/home');
 
-      cy.screenshot(`cadastro_usuario_${tipoUsuarioAleatorio}_sucesso`); 
+      cy.screenshot(`cadastro_usuario_${tipoUsuarioAleatorio}_sucesso`);
 
-  
-      delete usuarios[tipoUsuarioAleatorio]; 
- 
-      cy.writeFile('cypress/fixtures/usuarios.json', usuarios); 
+
+      delete usuarios[tipoUsuarioAleatorio];
+
+      cy.writeFile('cypress/fixtures/usuarios.json', usuarios);
     });
   });
 
